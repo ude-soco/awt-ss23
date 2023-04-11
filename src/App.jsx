dc8138dbd7178b5d8f4f11d6c69417b0ffef32a1
Shoeb Joarder <shoebjoarder@outlook.com>
2023-04-11 13:39:00 +0200

    states

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,74 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
 
-function App() {
-  const [count, setCount] = useState(10);
+// App component
+export default function App() {
+  const [count, setCount] = useState(7);
   const [name, setName] = useState("Counter App");
 
-  // console.log(count);
-  // console.log(name);
+  console.log(count);
+  console.log(name);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const changeName = () => {
+    setName("Counter Application");
+  };
+
+  console.log(count);
 
   return (
-    <div>
-      <h1> {name} </h1>
-      <h1> {count} </h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Grid
+        container
+        sx={{ p: 3, height: "100vh" }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            bgcolor: "#FFC53A",
+            maxWidth: 380,
+            minWidth: 380,
+            borderRadius: 3,
+            padding: 7,
+          }}
+        >
+          <Typography variant="h4" align="center" sx={{ mb: 6 }}>
+            {name}
+          </Typography>
+
+          <Typography variant="h1" sx={{ mb: 6 }}>
+            {count}
+          </Typography>
+          <Grid container justifyContent="center" spacing={1}>
+            <Grid item>
+              <Button variant="contained" onClick={increaseCount}>
+                Increase
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={changeName}
+              >
+                Change name
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 }
-
-export default App;
